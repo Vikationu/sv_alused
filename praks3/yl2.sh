b3d4075575165017b1bc40358283504136b63129
Raigo.Hoim <raigo.hoim@khk.ee>
2020-11-13 15:42:17 +0000

    Muutujate väärtus ja liitmine lauseks

---
+++ praks3/yl2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Muutujatele väärtus ja liitmine lauseks
+# author Raigo Hoim
+#
+arv=2016.
+president="Kersti Kaljulaid"
+keskosa="aastal valiti Eesti presidendiks"
+
+echo "$arv $keskosa $president"
+
+est="Eesti vabariigi president"
+nimi="Kersti Kaljulaid"
+f="on sündinud"
+aeg="30.dets 1969"
+koht="Tartus."
+
+echo "$est $nimi $f $aeg $koht"

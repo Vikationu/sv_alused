82296723492f7c93900cc5d110bb813d2e1a435e
Raigo.Hoim <raigo.hoim@khk.ee>
2020-11-21 10:09:02 +0000

    kellaaja kysimine ja vastav tervitus

---
+++ praks5/yl3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Vastavalt kellaaja tervitus
+# autor Raigo Hoim
+#
+echo -n "Sisesta aja vaartus: "
+read aeg
+if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
+	echo "Tere hommikust!"
+elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
+	echo "Tere paevast!"
+elif [ $aeg -ge 18 -a $aeg -lt 22 ]; then
+	echo "Tere Ohtust!"
+elif [ $aeg -ge 22 -a $aeg -lt 24 ] || [ $aeg -ge 0 -a $aeg -lt 6 ]; then
+	echo "Head ood!"
+else
+	echo "kontrolli et sisestasid oige aja"
+fi
